e60a4ecbfde049eb6dd5cbae49734af24303406a
HASEE <zhougongkai>
2019-07-07 16:43:11 +0800

    更新

--- src/main/java/com/shuzhi/websocket/WebSocketServer.java
+++ src/main/java/com/shuzhi/websocket/WebSocketServer.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.shuzhi.rabbitmq.Message;
 import com.shuzhi.rabbitmq.RabbitProducer;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
